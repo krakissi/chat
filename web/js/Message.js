e40ad3c38e466b140208339405678ce3f6d7bbeb
krakissi <mike@krakissi.net>
2014-02-09 12:10:34 -0500

    Basic Chat Layout
    A simple CSS layout has been added to mimic the old Krakchat setup. It's
    now possible to send and receive messages.

--- web/js/Message.js
+++ web/js/Message.js
@@ -1,5 +1,5 @@
 /*
-	Send messages.
+	Send and receive messages.
 */
 var Message = {
 	name: "Message",
@@ -7,8 +7,12 @@ var Message = {
 	message: undefined,
 
 	send: function(){
-		var post = "message="+encodeURIComponent(this.message.value);
-		this.message.value = "";
+		var post = encodeURIComponent(this.message.value);
+
+		if(post == "")
+			return false;
+
+		post = "message=" + post;
 
 		Ajax.get({
 			method:'POST',
@@ -21,17 +25,44 @@ var Message = {
 
 	// Write a received message out to the chatbody div.
 	receive: function(msg){
+		var locked = false;
+
+		if(Message.chatbody.scrollTop == (Message.chatbody.scrollHeight - Message.chatbody.offsetHeight))
+			locked = true;
+
 		if(msg.message){
 			msg.message = decodeURIComponent(msg.message);
 			this.chatbody.innerHTML +='<div><span class=timestamp>'+msg.timestamp
 				+'</span> <span class=user>'+msg.user+'</span>@<span class=remote_addr>'
 				+msg.remote_addr+'</span>: <span class=message>'+msg.message+'</span></div>';
 		}
+
+		if(locked)
+			Message.chatbody.scrollTop = Message.chatbody.scrollHeight;
+	},
+
+	// Handle received keyboard input.
+	keyhandler: function(event){
+		var key = event.keyCode || event.which;
+
+		if((!event.shiftKey) && (key == 13)){
+			switch(event.type){
+				case "keypress":
+					Message.send();
+					break;
+				case "keyup":
+					Message.message.value = "";
+					break;
+			}
+		}
 	},
 
 	init: function(){
 		this.chatbody = document.getElementById('chatbody');
 		this.message = document.getElementById('message');
+
+		this.message.onkeypress = this.keyhandler;
+		this.message.onkeyup = this.keyhandler;
 	}
 };
 
