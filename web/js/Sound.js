5f3b89d5f1764673f477b94ac135e3ab9f9d394c
krakissi <mike@krakissi.net>
2014-06-19 16:36:32 -0400

    Fix for chrome bloops.

--- web/js/Sound.js
+++ web/js/Sound.js
@@ -24,8 +24,13 @@ var Sound = {
 	},
 
 	messagealert: function(){
-		if(Sound.bloop.muted === false)
+		if(Sound.bloop.muted === false){
+			// Chrome workaround
+			if(window.chrome)
+				Sound.bloop.load();
+
 			Sound.bloop.play();
+		}
 		Sound.mute();
 	},
 
