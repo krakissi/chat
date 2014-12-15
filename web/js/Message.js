abca21a0ff6a6c19eac0172797320e17d13bfedc
krakissi <mike@krakissi.net>
2014-12-15 15:20:01 -0500

    Toggle snow on resize
    This is so very ugly.

--- web/js/Message.js
+++ web/js/Message.js
@@ -242,6 +242,12 @@ var Message = {
 		this.truncate();
 
 		document.addEventListener(Core.resizeevent.eventName, this.scrollbottom);
+
+		// YULE CRAP
+		document.addEventListener(Core.resizeevent.eventName, function(){
+			$("#toggle-snow").click();
+			$("#toggle-snow").click();
+		});
 	}
 };
 
